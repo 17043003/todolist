d7765ff69e63bfc3340c056f20019e40950c0d6d
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-09 22:10:45 +0900

    add callback when tasks name is none

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
+    before_validation :set_nameless_name
+
     validates :name, presence: true, length: { maximum: 30 }
+
+    
+    private
+    def set_nameless_name
+        self.name = 'タスク名なし' if name.blank?
+    end
 end
