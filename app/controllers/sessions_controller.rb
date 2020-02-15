51d4998350bbe5569339fca0985a15157bd0fcba
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-16 05:10:33 +0900

    implement create action in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    # パスワードを認証できればsessionにIDを格納する
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to :root, notice: 'ログインしました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
