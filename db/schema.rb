63df789703611c12b00388e551ae2b13bea7afd7
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-15 22:48:48 +0900

    add place column to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_114945) do
+ActiveRecord::Schema.define(version: 2020_02_15_134642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_02_12_114945) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "place"
   end
 
   create_table "users", force: :cascade do |t|
