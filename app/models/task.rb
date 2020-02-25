95cd8a7b1d285584f95ebaa87e998c39604e78aa
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-26 04:51:21 +0900

    limit search parameter

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,8 +8,16 @@ class Task < ApplicationRecord
     scope :recent_created, -> { order(created_at: :desc) }
     scope :recent_updated, -> { order(updated_at: :desc) }
 
+    def self.ransackable_attributes(auth_object = nil)
+      %w[name created_at]
+    end
+
+    def self.ransackable_associations(auth_object = nil)
+      []
+    end
+
     private
     def set_nameless_name
-        self.name = 'タスク名なし' if name.blank?
+      self.name = 'タスク名なし' if name.blank?
     end
 end
