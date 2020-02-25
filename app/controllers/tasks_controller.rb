afa72eb9c1885f8021eccca88c458582726585bf
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-26 05:20:41 +0900

    can upload image with active_storage

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   private
   
   def task_params
-    params.require(:task).permit(:name, :description, :place)
+    params.require(:task).permit(:name, :description, :image, :place)
   end
 
   def set_task
