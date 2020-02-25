078d38210a7bcf1e8729f792ea7d455ecc669480
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-26 06:11:16 +0900

    add done column to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_200522) do
+ActiveRecord::Schema.define(version: 2020_02_25_210948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_200522) do
     t.datetime "updated_at", null: false
     t.string "place"
     t.bigint "user_id", null: false
+    t.boolean "done", default: false, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
