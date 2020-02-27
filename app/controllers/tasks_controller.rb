2a8d9e289fa07c98494797502e8d484d9a5e9ce6
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-28 05:22:22 +0900

    can save deadline to task table

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
   private
   
   def task_params
-    params.require(:task).permit(:name, :description, :image, :place, :done)
+    params.require(:task).permit(:name, :description, :image, :place, :done, :deadline)
   end
 
   def set_task
