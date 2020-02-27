b86569c43b9c25e77719f651f34807788a7bd194
17043003 <35760051+17043003@users.noreply.github.com>
2020-02-28 05:16:45 +0900

    migrate add deadline column to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_210948) do
+ActiveRecord::Schema.define(version: 2020_02_27_201451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_210948) do
     t.string "place"
     t.bigint "user_id", null: false
     t.boolean "done", default: false, null: false
+    t.datetime "deadline"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
