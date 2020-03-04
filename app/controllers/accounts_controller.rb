19c3de5ce5521e970bffba8f95786b25a9b89eb3
17043003 <35760051+17043003@users.noreply.github.com>
2020-03-04 23:12:47 +0900

    can edit account info

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,5 +4,21 @@ class AccountsController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.assign_attributes(account_params)
+    if @user.save
+      redirect_to :account, notice: 'アカウント情報を編集しました'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def account_params
+    params.require(:account).permit(:name, :email)
   end
 end
