378733ff7ea3f051ff3549d1c44a4ee834cad643
17043003 <35760051+17043003@users.noreply.github.com>
2020-03-04 22:17:58 +0900

    generate accounts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'accounts/show'
+  get 'accounts/edit'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
