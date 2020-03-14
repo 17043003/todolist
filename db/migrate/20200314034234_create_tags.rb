d24b4ea3baf0c1d77fab49012f45b97576bca912
17043003 <35760051+17043003@users.noreply.github.com>
2020-03-14 12:45:39 +0900

    migrate add user references tag

---
+++ db/migrate/20200314034234_create_tags.rb
@@ -0,0 +1,10 @@
+class CreateTags < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tags do |t|
+      t.string :name
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
