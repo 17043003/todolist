a2232a7433a4d8e0a2afeb88204d2fe54fad51db
17043003 <35760051+17043003@users.noreply.github.com>
2020-03-14 12:49:58 +0900

    add has_many and belongs_to to each models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :tasks
+  has_many :tags
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
